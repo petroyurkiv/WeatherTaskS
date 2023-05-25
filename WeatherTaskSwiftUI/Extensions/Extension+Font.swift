f71c4e2feb16087acc764b0d7921ce8d7d3d4b79
petroyurkiv <igorlamativ@gmail.com>
2023-05-25 11:53:30 +0300

    font extensions has been added

---
+++ WeatherTaskSwiftUI/Extensions/Extension+Font.swift
@@ -0,0 +1,22 @@
+//
+//  Extension+Font.swift
+//  WeatherTaskSwiftUI
+//
+//  Created by Petro on 25.05.2023.
+//
+
+import SwiftUI
+
+extension Font {
+    static var title: Font {
+        Font.custom("Inter-SemiBold", size: 32)
+    }
+    
+    static var subtitle: Font {
+        Font.custom("Inter-SemiBold", size: 20)
+    }
+    
+    static var simpleText: Font {
+        Font.custom("Inter-SemiBold", size: 16)
+    }
+}
