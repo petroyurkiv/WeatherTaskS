d586e541a37c672b524d9ae13a09a0963a290078
petroyurkiv <igorlamativ@gmail.com>
2023-05-25 10:44:20 +0300

    Initial Commit

---
+++ WeatherTaskSwiftUI/WeatherTaskSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTaskSwiftUIApp.swift
+//  WeatherTaskSwiftUI
+//
+//  Created by Petro on 25.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherTaskSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
